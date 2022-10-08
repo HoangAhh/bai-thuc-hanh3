3af7cb9923831940475acbaf8e1cc7ef22e23ebb
jlh7 <j.lh.t@outlook.com>
2022-10-08 21:25:43 +0700

    Tuan

--- src/bai_thuc_hanh3/PhepCong_HoangTuan.java
+++ src/bai_thuc_hanh3/PhepCong_HoangTuan.java
@@ -1,13 +1,7 @@
 package bai_thuc_hanh3;
 
 public class PhepCong_HoangTuan {
-	private int a;
-	private int b;
-	public PhepCong_HoangTuan(int a, int b) {
-		this.a = a;
-		this.b = b;
-	}
-	public int getKetQua() {
-		return this.a + this.b;
+	int phepCong(int a, int b) {
+		return a + b;
 	}
 }
