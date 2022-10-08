16182f047bd38c71e01610b52f36bf9ce7733bf3
phanbao569 <phanbao569@example.com>
2022-10-08 21:31:28 +0700

    refactoting

--- src/bai_thuc_hanh3/Calculator.java
+++ src/bai_thuc_hanh3/Calculator.java
@@ -1,7 +1,14 @@
 package bai_thuc_hanh3;
 
 public class Calculator {
+
+	
 	public static void main(String[] args) {
+		Calculator y = new Calculator();
 		System.out.println("Hihi");
+		System.out.println(" moi ban nhap 2 so");
+		System.out.println(" moi ban phep tinh");
+		
+		
 	}
 }
--- src/bai_thuc_hanh3/PhepNhan_ThanhBao.java
+++ src/bai_thuc_hanh3/PhepNhan_ThanhBao.java
@@ -1,7 +1,15 @@
+/**
+ * 
+ */
+/**
+ * @author Bao Phan
+ */
 package bai_thuc_hanh3;
-
 public class PhepNhan_ThanhBao {
- int nhan(int a, int b) {
+	// TODO ham nhan
+	// sua int thanh long
+ long nhan(int a, int b) {
+	 // ko cần biến c, cho trả về giá trị a*b
 	 return a*b;
  }
 }
