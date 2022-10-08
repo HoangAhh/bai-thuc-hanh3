a0f5d5abc58f6d70fc744d8665277b2affce9b3b
jlh7 <j.lh.t@outlook.com>
2022-10-08 21:17:05 +0700

    Tuan lam phan cong nha

--- src/bai_thuc_hanh3/Calculator.java
+++ src/bai_thuc_hanh3/Calculator.java
@@ -3,5 +3,6 @@ package bai_thuc_hanh3;
 public class Calculator {
 	public static void main(String[] args) {
 		System.out.println("Hihi");
+		System.out.println((new PhepCong_HoangTuan(4, 7)).getKetQua());
 	}
 }
---
+++ src/bai_thuc_hanh3/PhepCong_HoangTuan.java
@@ -0,0 +1,13 @@
+package bai_thuc_hanh3;
+
+public class PhepCong_HoangTuan {
+	private int a;
+	private int b;
+	public PhepCong_HoangTuan(int a, int b) {
+		this.a = a;
+		this.b = b;
+	}
+	public int getKetQua() {
+		return this.a + this.b;
+	}
+}
